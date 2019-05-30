582e4c006bfe03ee88bec4017950100447a8c2f3
jonzhang <jon.zhang@trustlife.com>
2019-05-30 11:20:41 +0800

    1,uivew

--- YZCoderLearn/YZCoderLearn/ViewController.m
+++ YZCoderLearn/YZCoderLearn/ViewController.m
@@ -8,6 +8,25 @@
 
 #import "ViewController.h"
 
+@interface TestView : UIView
+
+@end
+@implementation TestView
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview {
+    [super willMoveToSuperview:newSuperview];
+}
+- (void)didMoveToSuperview {
+    [super didMoveToSuperview];
+}
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow {
+    [super willMoveToWindow:newWindow];
+}
+- (void)didMoveToWindow {
+    [super didMoveToWindow];
+}
+
+@end
 @interface ViewController ()
 
 @end
@@ -17,6 +36,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self.view addSubview:({
+        TestView * view = [[TestView alloc] init];
+        view.frame = CGRectMake(150, 50, 100, 100);
+        view.backgroundColor = [UIColor redColor];
+         view;
+    } )];
 }
 
 
