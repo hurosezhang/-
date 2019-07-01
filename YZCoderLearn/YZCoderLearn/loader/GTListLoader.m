1181ab2b9d903a3120a6bbbc1b1b665f799e11d4
jonzhang <jon.zhang@trustlife.com>
2019-07-01 22:23:43 +0800

    pod 和 json 解析

--- YZCoderLearn/YZCoderLearn/loader/GTListLoader.m
+++ YZCoderLearn/YZCoderLearn/loader/GTListLoader.m
@@ -7,18 +7,32 @@
 //
 
 #import "GTListLoader.h"
-
+#import <AFNetworking/AFNetworking.h>
 @implementation GTListLoader
 - (void)loadlistData {
     NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
     NSURL *listURL = [NSURL URLWithString:urlString];
-//    NSURLRequest * listRequest = [[NSURLRequest alloc]initWithURL:listURL ];
+
+//    [[AFHTTPSessionManager manager] GET:urlString parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+//
+//    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//
+//    }];
+    
+    
+    NSURLRequest * listRequest = [[NSURLRequest alloc]initWithURL:listURL ];
     
     NSURLSession * session = [NSURLSession sharedSession];
     NSURLSessionDataTask * dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        NSError *jsonError ;
+        id jsonObj = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+//        NSLog(@"%@",jsonObj);
         
     }];
-    
+
     [dataTask resume];
     
 }
