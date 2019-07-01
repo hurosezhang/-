91cfd8130f363aebc01a78ef754a8fd55bae1e0b
jonzhang <jon.zhang@trustlife.com>
2019-07-01 15:18:39 +0800

    nsurlsessiontask

--- YZCoderLearn/YZCoderLearn/loader/GTListLoader.m
+++ YZCoderLearn/YZCoderLearn/loader/GTListLoader.m
@@ -12,6 +12,15 @@
 - (void)loadlistData {
     NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
     NSURL *listURL = [NSURL URLWithString:urlString];
+//    NSURLRequest * listRequest = [[NSURLRequest alloc]initWithURL:listURL ];
+    
+    NSURLSession * session = [NSURLSession sharedSession];
+    NSURLSessionDataTask * dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+    }];
+    
+    [dataTask resume];
+    
 }
 
 @end
